5a145b43ef31ed597fc19c010eccd2fce2f6235d
jasmin <jasmin.fazlic@tuwien.ac.at>
2016-12-18 18:33:12 +0100

    add source_field (needed) just override getAmount() function

--- Model/Sales/Pdf/Tax.php
+++ Model/Sales/Pdf/Tax.php
@@ -22,23 +22,13 @@ namespace MSP\CashOnDelivery\Model\Sales\Pdf;
 
 class Tax extends \Magento\Sales\Model\Order\Pdf\Total\DefaultTotal
 {
-    public function getTotalsForDisplay()
+    /**
+     * Get Total amount from source
+     *
+     * @return float
+     */
+    public function getAmount()
     {
-        $order = $this->getOrder();
-        $amount = $order->formatPriceTxt($order->getBaseMspCodTaxAmount());
-        if ($this->getAmountPrefix()) {
-            $amount = $this->getAmountPrefix() . $amount;
-        }
-
-        $title = __($this->getTitle());
-        if ($this->getTitleSourceField()) {
-            $label = $title . ' (' . $this->getTitleDescription() . '):';
-        } else {
-            $label = $title . ':';
-        }
-
-        $fontSize = $this->getFontSize() ? $this->getFontSize() : 7;
-        $total = ['amount' => $amount, 'label' => $label, 'font_size' => $fontSize];
-        return [$total];
+        return $this->getOrder()->getBaseMspCodTaxAmount();
     }
 }
