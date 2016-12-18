74483f0411b593e104ec77094a0e147647141a34
jasmin <jasmin.fazlic@tuwien.ac.at>
2016-12-18 18:12:50 +0100

    include cod in pdf invoice

---
+++ Model/Sales/Pdf/Tax.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * IDEALIAGroup srl
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@idealiagroup.com so we can send you a copy immediately.
+ *
+ * @category   MSP
+ * @package    MSP_CashOnDelivery
+ * @copyright  Copyright (c) 2016 IDEALIAGroup srl (http://www.idealiagroup.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace MSP\CashOnDelivery\Model\Sales\Pdf;
+
+class Tax extends \Magento\Sales\Model\Order\Pdf\Total\DefaultTotal
+{
+    public function getTotalsForDisplay()
+    {
+        $order = $this->getOrder();
+        $amount = $order->formatPriceTxt($order->getBaseMspCodTaxAmount());
+        if ($this->getAmountPrefix()) {
+            $amount = $this->getAmountPrefix() . $amount;
+        }
+
+        $title = __($this->getTitle());
+        if ($this->getTitleSourceField()) {
+            $label = $title . ' (' . $this->getTitleDescription() . '):';
+        } else {
+            $label = $title . ':';
+        }
+
+        $fontSize = $this->getFontSize() ? $this->getFontSize() : 7;
+        $total = ['amount' => $amount, 'label' => $label, 'font_size' => $fontSize];
+        return [$total];
+    }
+}
